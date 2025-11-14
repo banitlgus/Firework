079105953c7d141b56df14a8ed63f337588ab2d7
banitlgus <banitlgus@gmail.com>
2025-11-14 10:38:39 +0900

    test
    tested how it runs without .copy

--- Particle.js
+++ Particle.js
@@ -2,7 +2,7 @@ class Particle {
     constructor(position) {
         this.acceleration = createVector(0, 0.05);
         this.velocity = createVector(random(-1, 1), random(-1, 0));
-        this.position = position.copy();
+        this.position = position;
         this.lifespan = 255;
     }
 
