04fc07d97559ed25c814fdb6301f9605c30a7bd3
banitlgus <banitlgus@gmail.com>
2025-11-14 12:04:45 +0900

    add array
    add array
    change the way of mouse interaction

--- sketch.js
+++ sketch.js
@@ -1,20 +1,33 @@
 let system;
+let systems = [];
 
 function setup() {
   createCanvas(720, 400);
-  system = new ParticleSystem(createVector(width / 2, 50));
+  // system = new ParticleSystem(createVector(width / 2, 50));
 }
 
 function draw() {
   background(51);
-  // system.addParticle();
-  // system.run();
-  system.setPosition(createVector(mouseX, mouseY));
 
-  if(mouseIsPressed) {
-      system.addParticle();
-      system.run();
-      let force = createVector(0, 0.05);
-      system.addForce(force);
+  for (let s of systems){
+    s.addParticle();
+    s.run();
+    // s.setPosition(createVector(mouseX, mouseY));
   }
+  // system.setPosition(createVector(mouseX, mouseY));
+
+  // if(mouseIsPressed) {
+  //     let s = new ParticleSystem(createVector(width/2, 50));
+  //     systems.push(s);
+  //     // system.addParticle();
+  //     // system.run();
+  //     let force = createVector(0, 0.05);
+  //     s.addForce(force);
+  // }
+}
+
+function mouseClicked() {
+  let s = new ParticleSystem(createVector(width/2, 50));
+  systems.push(s);
+  s.setPosition(createVector(mouseX, mouseY));
 }
